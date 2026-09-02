fc8968a7ae4f7d058d1e538c0ef4aaa85e829251
Carlos-D04 <carloskrl123@gmail.com>
2026-09-02 20:19:38 -0300

    add roles and department to user

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,23 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    ...
+
+    class Role(models.TextChoices):
+        CITIZEN = "CITIZEN", "Citizen"
+        STAFF = "STAFF", "Staff"
+        ADMIN = "ADMIN", "Admin"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CITIZEN
+        )
+
+
+    
+    department = models.ForeignKey(
+        "departments.Department",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True
+        )
