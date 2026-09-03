afcea8c5691771f31f2e1c30ca441028361dbb13
Carlos-D04 <carloskrl123@gmail.com>
2026-09-02 22:37:32 -0300

    add request serializer

---
+++ requests/serializers.py
@@ -0,0 +1,13 @@
+from models import Request
+from rest_framework import serializers
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = ['title', 'description', 'location', 'category', 'author', 'department', 'status', 'crated_at', 'updated_at']
+        read_only_fields = ['author', 'department', 'status', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        category = validated_data["category"]
+
+        return Request.objects.create(department=category.department, **validated_data)
