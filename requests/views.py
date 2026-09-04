28cb58026744ba25a3cbe4e2024eafa922d72d75
Carlos-D04 <carloskrl123@gmail.com>
2026-09-03 21:35:58 -0300

    add request status transitions rules and history

--- requests/views.py
+++ requests/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
-from .models import Request
+from .models import Request, RequestHistory
 from .serializers import RequestSerializer
 from .permissions import RequestPermission
 
@@ -10,7 +13,6 @@ from .permissions import RequestPermission
 class RequestViewSet(viewsets.ModelViewSet):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
-
     permission_classes = [IsAuthenticated, RequestPermission]
 
     def perform_create(self, serializer):
@@ -25,3 +27,29 @@ class RequestViewSet(viewsets.ModelViewSet):
         if user.role == user.Role.ADMIN:
             return Request.objects.all()
         return Request.objects.none()
+
+    @action(detail = True, methods=["patch"], url_path="change-status")
+    def change_status(self, request, pk=None):
+        # order of the next status transitions allowed.
+        allowed_transitions = { 
+            Request.Status.PENDING : Request.Status.IN_REVIEW,
+            Request.Status.IN_REVIEW: Request.Status.IN_PROGRESS,
+            Request.Status.IN_PROGRESS: Request.Status.RESOLVED
+        }
+        request_obj = self.get_object() #ID from URL
+
+        old_status = request_obj.status
+        new_status = request.data.get("status")
+        next_status = allowed_transitions.get(old_status)
+
+        if new_status != next_status:
+            return Response({"detail": "Invalid status transition"}, status=status.HTTP_400_BAD_REQUEST)
+
+        request_obj.status = next_status
+        request_obj.save()
+
+        RequestHistory.objects.create(request = request_obj, old_status = old_status, new_status = next_status, changed_by = request.user)
+
+        return Response({"detail": "Status updated successfully"}, status=status.HTTP_200_OK)
+
+    
