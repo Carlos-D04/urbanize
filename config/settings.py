4db7b90a98d949f4cc5dea72d185989fa8fc5791
Carlos-D04 <carloskrl123@gmail.com>
2026-09-11 00:45:41 -0300

    move secret key to enviroment variable

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%yr(-bq8n62@yp=(tv4*$b)g78sn0of8hu@%ucp%-1p80pc^9'
+SECRET_KEY = 'os.getenv("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
